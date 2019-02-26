af36f0778406af97b6ec4c7b64d4025153214bbc
Ryan Gale <ryanwfgale@gmail.com>
2019-02-26 16:16:49 -0700

    add MVC and frameworks

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,37 @@
+class AppointmentsController < ApplicationController
+  before_action :set_physician
+
+  def index
+    @appointments = @physician.appointments.order(:date, :time)
+  end
+
+
+    def new
+      @physicians = Physician.all - @physician.patients
+      @appointment = @physician.appointments.new
+    end
+
+
+  def create
+    @appointment = @physician.appointments.new(appointment_params)
+    if @appointment.save
+      redirect_to physician_appointments_path(@physician)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @physician.appointments.find(params[:id]).destroy
+    redirect_to physician_appointments_path
+  end
+
+ private
+   def set_physician
+     @physician = Physician.find(params[:physician_id]) 
+   end
+
+   def appointment_params
+     params.require(:appointment).permit(:name, :patient_id, :date, :time)
+   end
+end
